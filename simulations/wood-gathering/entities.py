10176ec6c9325c7476733af2319ee2c9c0595ffe
Dominic Davies <dominictdavies@gmail.com>
2025-01-16 01:01:00 +0800

    Move Item into entities

--- simulations/wood-gathering/entities.py
+++ simulations/wood-gathering/entities.py
@@ -1,6 +1,12 @@
-from item import Item
-import random
 import pygame
+import random
+
+
+class Item:
+    def __init__(self, name: str, max_stack: int, stack: int = 0):
+        self.name = name
+        self.max_stack = max_stack
+        self.stack = stack
 
 
 class Player:
--- simulations/wood-gathering/item.py
+++
@@ -1,5 +0,0 @@
-class Item:
-    def __init__(self, name: str, max_stack: int, stack: int = 0):
-        self.name = name
-        self.max_stack = max_stack
-        self.stack = stack
