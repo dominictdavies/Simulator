1124b60975e5e2032c876a65ab5f0e48c007f0bf
Dominic Davies <dominictdavies@gmail.com>
2025-01-16 03:21:02 +0800

    Add active trees and carts

--- simulations/wood-gathering/simulate.py
+++ simulations/wood-gathering/simulate.py
@@ -13,9 +13,13 @@ def main():
     pygame.display.set_caption("Wood Gathering")
 
     # Entity sets
-    players: list[Player] = []
-    trees: list[Tree] = []
-    carts: list[Cart] = []
+    players: list[Player] = [Player([width // 2, height // 2])]
+    trees: list[Tree] = [
+        Tree([width // 2, height // 3]),
+        Tree([width // 3, height // 3 * 2]),
+        Tree([width // 3 * 2, height // 3 * 2]),
+    ]
+    carts: list[Cart] = [Cart([width // 2, height // 2])]
 
     # Game loop
     while True:
@@ -25,14 +29,21 @@ def main():
                 pygame.quit()
 
         # Update
-        players.append(Player([width / 2, height / 2]))
         for player in players:
             player.ai()
+        for tree in trees:
+            tree.ai()
+        for cart in carts:
+            cart.ai()
 
         # Draw
         screen.fill((0, 0, 0))
         for player in players:
             player.draw(screen)
+        for tree in trees:
+            tree.draw(screen)
+        for cart in carts:
+            cart.draw(screen)
         pygame.display.update()
 
 
